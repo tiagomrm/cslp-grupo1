5ef392703054aea30da955f56bed8e581600fd03
Vitor <vfrd00@ua.pt>
2022-01-27 18:58:04 +0000

    Project cleaning and README update

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# cslp-grupo1
+# CSLP Project | Group 1
+
+Lego Blocks Color and Dimensions Detection
+
+This project is able to detect an aproximation to the color of a Lego Block, based on an official color pallet, and it's dimensions (2x8, p.e.) using a camera.
+The code is in c++ with the OpenCV library in order to manipulate and capture the frames.
+
+We didn't manage to make the project working 100%, because the environment conditions shoul've been controled since the luminosity, wich we expected we could control, had way more influence than we thought.
+With that, we may now control the environment and make the project more reliable
+
+Project files:
+    - colors.hpp : Dataset with the colors
+    - main.cpp  : Main program containing all code and functions
+
+
+## How to install:
+
+In order to run the program, the Raspberry Pi needs OpenCV library.
+This is the guide we used : https://solarianprogrammer.com/2019/09/17/install-opencv-raspberry-pi-raspbian-cpp-python-development/
+
+## How to run
+To run the program, after having OpenCV library installed:
+    ```
+    $ g++ main.cpp -o readLego `pkg-config --cflags --libs opencv
+    ```
+    ```
+    $ ./readLego
+    ```
+
+To see our setup and indepth explanation check the report.
+
+
+## Authors:
+    - Tiago Matos nmec 98134
+    - Vítor Dias nmec 98396
--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// 98134 - Tiago Matos && 98396 - Vítor Dias @ CSLP
+
 #include <iostream>
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
@@ -5,56 +7,62 @@
 #include <opencv2/photo.hpp>
 #include <opencv2/core/types.hpp>
 
+//Import color dataset
 #include "colors.h"
+
+//Use std and opencv namespaces
 using namespace cv;
 using namespace std;
 
+//Global Variables
 Mat src_gray;
 int thresh = 100;
 RNG rng(12345);
+int width;
+int height;
 
-vector<Point2f> getDimensions(vector<Vec3f> circles, int mX, int mY);
+//----
+//Functions Declarations
 
+//Get Lego Brick size
+void getDimensions(vector<Vec3f> circles, int mX, int mY);
+
+//Get Bounding Rectangles from objects in the image
 vector<Rect> getContours(Mat m);
 
+//Check if circles are inside one of the detected rectangles
 int insideRect(vector<Vec3f> circles, vector<Rect> rects);
 
+//Get points from arrow equation
 double f(double x, double slope, double b);
 
+//
 void white_balance(Mat& src, Mat& dst, Mat& mask);
+//-----
 
- //   putText(src, "color: ", Point(30,30), FONT_HERSHEY_DUPLEX, 0.6, Scalar(0,0,0), 1);
-//    putText(src, max > 0 ? toUpperCase(colors_names[index]) : "Unknown", Point(30,60), FONT_HERSHEY_DUPLEX, 0.8, Scalar(0,0,0), 2);
-//    imshow("original image", src);
 
 int main(int argc, char** argv)
 {
-    
+
     // Declare the output variables
-    Mat dst, dst2, th, masked, img_gray, result, end, color, color_seg, grey, white_balanced;
-    const char* default_file = "legos.jpg";
-    //default_file = "lego3.png";
-    const char* filename = argc >=2 ? argv[1] : default_file;
-    // Loads an image
-    Mat src = imread( samples::findFile( filename ), IMREAD_COLOR );
+    Mat src, dst, dst2, th, masked, img_gray, result, end, color, color_seg, grey, white_balanced;
 
-    float scale = 1;
+    //Get Camera instance
+    VideoCapture video(0);
 
-    if (strcmp(default_file,"lego3.png") != 0)
-        scale = 0.6;
-
-    // Check if image is loaded fine
-    if(src.empty()){
-        printf(" Error opening image\n");
-        printf(" Program Arguments: [image_name -- default %s] \n", default_file);
+    //Make sure the Camera is working
+    if (!video.isOpened()){
+        cout << "Camera not detected" << endl;
         return -1;
     }
 
-    //VideoCapture video(0);
+    //Capture Camera frame
+    video >> src;
 
-    //video >> src;
-
+    //Convert color from BGR to Gray Scale
     cvtColor(src, img_gray, COLOR_BGR2GRAY);
+
+    //
     cv::threshold(img_gray, th, 0, 255, THRESH_TOZERO | THRESH_OTSU);
 
     white_balance(src, white_balanced, th);
@@ -72,137 +80,69 @@ int main(int argc, char** argv)
     cv::bitwise_and(src, src, masked, th);
     cv::pyrMeanShiftFiltering(masked, dst2, 40, 55, 2);
 
-    // Show results
-
-    namedWindow("Original", WINDOW_NORMAL);
-    resizeWindow("Original", 1000,1000);
-    imshow("Original", src);
-//
-//    namedWindow("Gaussian Blur", WINDOW_NORMAL);
-//    resizeWindow("Gaussian Blur", 1000,1000);
-//    imshow("Gaussian Blur", img_gray);
-//
-//    namedWindow("Threshold", WINDOW_NORMAL);
-//    resizeWindow("Threshold", 1000,1000);
-//    imshow("Threshold", th);
-//
-    namedWindow("Masked", WINDOW_NORMAL);
-    resizeWindow("Masked", 1000,1000);
-    imshow("Masked", masked);
-//
-    namedWindow("Mean-Shift Segmentation", WINDOW_NORMAL);
-    resizeWindow("Mean-Shift Segmentation", 1000,1000);
-    imshow("Mean-Shift Segmentation", dst2);
-
-
-    //Get actual piece position
+ 
+    //Clone masked image to find the pins
     Mat pins = masked.clone();
 
+    //
     fastNlMeansDenoising(pins,pins,10.1,10,7);
 
-    //resize(pins, pins, Size(), scale, scale, INTER_LINEAR);
-
-//    namedWindow("denoise", WINDOW_NORMAL);
-//    resizeWindow("denoise", 1000,1000);
-//    imshow("denoise", pins);
-
-
-
-
-
+    //Convert image color from HSV to Gray Scale
     cvtColor(pins, pins, COLOR_HSV2BGR);
-
-
-//    Mat dest = Mat::zeros( pins.size(), pins.type() );
-//    double alpha = 4; /*< Simple contrast control */
-//    int beta = 0;       /*< Simple brightness control */
-//    for( int y = 0; y < pins.rows; y++ ) {
-//        for( int x = 0; x < pins.cols; x++ ) {
-//            for( int c = 0; c < pins.channels(); c++ ) {
-//                dest.at<Vec3b>(y,x)[c] =
-//                  saturate_cast<uchar>( alpha*pins.at<Vec3b>(y,x)[c] + beta );
-//            }
-//        }
-//    }
-//
-//    pins = dest;
-
-
     cvtColor(pins,src_gray, COLOR_BGR2GRAY);
 
-
+    //
     medianBlur(src_gray, src_gray, 5);
-
+    
+    //
     equalizeHist( src_gray, src_gray );
-    namedWindow("hist", WINDOW_NORMAL);
-    resizeWindow("hist", 1000,1000);
-    imshow("hist", src_gray);
+
 
     vector<Rect> rectangles;
     rectangles = getContours(src);
 
 
-    //namedWindow("cont", WINDOW_NORMAL);
-    //resizeWindow("cont", 1000,1000);
-    //imshow("cont", drawing);
-
-    // 50, 35, 10, 25, 35
-
+    // Find Circles in the image and save them in "circles"
     vector<Vec3f> circles;
     HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1,
-                 150  ,  // change this value to detect circles with different distances to each other
-                 20, 20, 60, 70 // change the last two parameters
-            // (min_radius & max_radius) to detect larger circles
+                 50,  
+                 10, 15, 15, 25
     );
 
+    // Save circles found as "circle" structure
     for( size_t i = 0; i < circles.size(); i++ )
     {
         Vec3i c = circles[i];
         Point center = Point(c[0], c[1]);
         // circle center
-        circle( pins, center, 1, Scalar(0,100,100), 3, LINE_AA);
+        circle( pins, center, 1, Scalar(0,100,100), 2, LINE_AA);
         // circle outline
         int radius = c[2];
-        circle( pins, center, radius, Scalar(255,0,255), 3, LINE_AA);
+        circle( pins, center, radius, Scalar(255,0,255), 2, LINE_AA);
     }
 
+    // Get the dimensions of the Lego Brick
+    getDimensions(circles, pins.cols, pins.rows);  
 
-    vector<Point2f> extr = getDimensions(circles, pins.cols, pins.rows);  
-
-
-
-    line( pins,
-        extr.at(0),
-        extr.at(2),
-        Scalar( 255, 0, 0 ),
-        2,
-        LINE_8 );
-
-    line( pins,
-        extr.at(0),
-        extr.at(3),
-        Scalar( 255, 0, 0 ),
-        2,
-        LINE_8 );
-
+    // Find the rectangle that has more circles inside him
     int rect_piece = insideRect( circles , rectangles);
 
-    cout << "Index = " << rect_piece << endl;
+    //Cut the rectangle that has most circles, used to visualize the cricles found
+    //pins = pins(rectangles.at(rect_piece));
 
-    pins = pins(rectangles.at(rect_piece));
+    // Cut the image that is going to be analyzed to find the colour, in order to get only the part that contains most circles
+    dst2 = dst2(rectangles.at(rect_piece));
 
-    Mat other;
-    //dst2 = dst2(rectangles.at(rect_piece));
-    imshow("aaaaaaaa", dst2);
+    // See detected circles in the Lego Brick
+    //namedWindow("detected circles", WINDOW_NORMAL);
+    //resizeWindow("detected circles", 1000,1000);
+    //imshow("detected circles", pins);
 
-    namedWindow("detected circles", WINDOW_NORMAL);
-    resizeWindow("detected circles", 1000,1000);
-    imshow("detected circles", pins);
 
-    //Color Detection
+
+    // -- Color Detection --
     vector<int> maxes;
     int max = 0, index;
-
 
     //-----------
     for(int i = 0; i < colors_names.size() - 1; i++){
@@ -227,18 +167,10 @@ int main(int argc, char** argv)
             }
     }
 
-    imshow("aaa depression", color);
-
-    cv::cvtColor(dst2, dst2, COLOR_HSV2BGR);
-    imshow("Mean-Shift Segmentation", dst2);
-
-
     Mat element = getStructuringElement( MORPH_RECT,
                                          Size( 21, 21 ),
                                          Point( 10, 10 ) );
-    namedWindow("Color segmentation", WINDOW_NORMAL);
-    resizeWindow("Color segmentation", 1000,1000);
-    imshow("Color segmentation", color);
+
 
     cv::dilate(color, color, element);
     cv::erode(color, color, element);
@@ -246,106 +178,42 @@ int main(int argc, char** argv)
     cv::erode(color, color, element);
     cv::dilate(color, color, element);
 
-    //src = src(rectangles.at(rect_piece));
-    bitwise_and(src, src, color_seg, color);
 
+    //Console output
     cout << "max = " << max << ", index = " << index << endl;
     cout << "color is " << colors_names[index] << endl;
-    namedWindow("Segmentation", WINDOW_NORMAL);
-    resizeWindow("Segmentation", 1000,1000);
-    imshow("Segmentation", color_seg);
+
     if(!grey.empty()) {
-        imshow("Grey", grey);
+        //imshow("Grey", grey);
     }
 
+
+    // Convert source image back to BGR
+    cv::cvtColor(src, src, COLOR_HSV2BGR);
     
-    int dilation_size = 50;
-    Mat dilation_dst;
-    Mat element2 = getStructuringElement( MORPH_CROSS,
-                        Size( 2*dilation_size + 1, 2*dilation_size+1 ),
-                        Point( dilation_size, dilation_size ) );
-    dilate( color_seg, dilation_dst, element2 );
+    
+    // Insert data found in the original image to provide output    
+    putText(src, "color: ", Point(30,30), FONT_HERSHEY_DUPLEX, 0.6, Scalar(0,0,0), 1);
+    putText(src, max > 0 ? toUpperCase(colors_names[index]) : "Unknown", Point(30,60), FONT_HERSHEY_DUPLEX, 0.8, Scalar(0,0,0), 2);
+    putText(src, "dimensions : ", Point(30,90), FONT_HERSHEY_DUPLEX, 0.6, Scalar(0,0,0), 1);
+    putText(src, to_string(width) + "x" + to_string(height) , Point(30,120), FONT_HERSHEY_DUPLEX, 0.6, Scalar(0,0,0), 1);
 
-    namedWindow("Final", WINDOW_NORMAL);
-    resizeWindow("Final", 1000,1000);
-    imshow("Final", dilation_dst);
-
-
-    Mat final;
-    vector<vector<Point> > contours2;
-    cvtColor(color_seg, color_seg, COLOR_RGB2GRAY);
-    namedWindow("Final2", WINDOW_NORMAL);
-    resizeWindow("Final2", 1000,1000);
-    imshow("Final2", color_seg);
-    findContours( color_seg, contours2, RETR_TREE, CHAIN_APPROX_SIMPLE );
-    cout << contours2.size() << endl;
-//    for (int i = 0; i< contours2.size(); i++){
-//  
-//        if (i == 0){
-//            continue;
-//        }
-//  
-//
-//        vector<Point> contours_poly;
-//        approxPolyDP( contours2.at(i), contours_poly, 3, true );
-//
-//
-//        drawContours(final, contours_poly, 0, (0, 0, 255), 5);
-//  
-//        Moments mo = moments(contours2.at(i));
-//        
-//
-//
-//        if (mo.m00 != 0.0){
-//            int x = (int)(mo.m10/mo.m00);
-//            int y = (int)(mo.m01/mo.m00);
-//        }
-//
-//        if (contours_poly.size() == 3){
-//            //cv2.putText(img, 'Triangle', (x, y),
-//            //            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-//            cout << "triangle" << endl;
-//        }
-//        else if ( contours_poly.size() == 4){
-//            //cv2.putText(img, 'Quadrilateral', (x, y),
-//            //            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-//            cout << "rectangle" << endl;
-//        }
-//        else if(contours_poly.size() == 5){
-//            //cv2.putText(img, 'Pentagon', (x, y),
-//            //            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-//            cout << "pentagon" << endl;
-//
-//        }
-//        else if (contours_poly.size() == 6){
-//            //putText(img, 'Hexagon', (x, y),
-//            //            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-//            cout << "hexagon" << endl;
-//        }
-//        else{
-//            //putText(img, 'circle', (x, y),
-//            //            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-//            cout << "circle" << endl;
-//        }
-//    }
-
+    // Show the final result
+    imshow("Final Output", src);
+    
     // Wait and Exit
     waitKeyEx();
     return 0;
 }
 
-vector<Point2f> getDimensions(vector<Vec3f> circles, int mX, int mY)
+void getDimensions(vector<Vec3f> circles, int mX, int mY)
 {
-
-
-    Point2f minX(mX,0), maxX(0,0), minY(0,mY), maxY(0,0);
+    // Set the extreme points to find maximum X and Y and to find minimum Y
+    Point2f minX(mX,0), minY(0,mY), maxY(0,0);
+    // Search the circles to set the extremes
     for(int i = 0; i < circles.size(); i++)
     {
         Point2f p(circles.at(i)[0], circles.at(i)[1]);
-        if( p.x > maxX.x){
-            Point2f pf(p.x,p.y);
-            maxX = pf;
-        }
         if(p.x < minX.x){
             Point2f pf(p.x,p.y);
             minX = pf;
@@ -360,20 +228,14 @@ vector<Point2f> getDimensions(vector<Vec3f> circles, int mX, int mY)
         }
     }
 
-    vector<Point2f> vPoints(4);
-    vPoints[0] = minX;
-    vPoints[1] = maxX;
-    vPoints[2] = minY;
-    vPoints[3] = maxY;
-
-
-    cout << minX.y << ","<< minX.x << endl;
-    cout << maxY.y << "," << maxY.x << endl;
+    // Get arrow parameters
     double d1 = (minX.y - maxY.y)/(minX.x - maxY.x);
     double d2 = (minX.y - minY.y)/(minX.x - minY.x);
     double b1 = minX.y - (d1 * minX.x);
     double b2 = minX.y - (d2 * minX.x);
     double d;
+
+    // Count the number of circles where one point intersepts the arrow at the x where the arrow intersepts the circle center
     int l1 = 0, l2 = 0;
     for(int i = 0; i < circles.size(); i++){
         int y = f(circles.at(i)[0], d1, b1);
@@ -387,13 +249,17 @@ vector<Point2f> getDimensions(vector<Vec3f> circles, int mX, int mY)
         }
     }
 
-    return vPoints;
+    // Set Lego dimensions
+    width = l1;
+    height = l2;
 }
 
+// Calculate arrow Y's
 double f(double x, double slope, double b){
     return x * slope + b;
 }
 
+// Get the objects found at the image
 vector<Rect> getContours(Mat m){
     Mat src = m;
     Mat th;
@@ -408,20 +274,18 @@ vector<Rect> getContours(Mat m){
         approxPolyDP( contours[i], contours_poly[i], 3, true );
         boundRect[i] = boundingRect( contours_poly[i] );
     }
-    Mat drawing = Mat::zeros( th.size(), CV_8UC3 );
-    for( size_t i = 0; i< contours.size(); i++ )
-    {
-        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
-        drawContours( drawing, contours_poly, (int)i, color );
-        rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2 );
-    }
+    //Mat drawing = Mat::zeros( th.size(), CV_8UC3 );
+    //for( size_t i = 0; i< contours.size(); i++ )
+    //{
+    //    Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+    //    drawContours( drawing, contours_poly, (int)i, color );
+    //    rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2 );
+    //}
 
-    namedWindow("draw", WINDOW_NORMAL);
-    resizeWindow("draw", 1000,1000);
-    imshow("draw", drawing);
     return boundRect;
 }
 
+// Check which bounding rectangle has more circles and return his index
 int insideRect(vector<Vec3f> circles, vector<Rect> rects){
     int index = 0, max = 0;
     int counter;
